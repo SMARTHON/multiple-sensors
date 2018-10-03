aa07968aa5ffa3dcb564e79632f5ce6ed710ffe3
Ken <Ken>
2018-10-03 13:16:48 +0800

    Add the output block

--- sensors.ts
+++ sensors.ts
@@ -46,6 +46,7 @@ namespace Sensors {
     //% blockId="smarthon_get_light" 
     //% block="Get light (Lx)"
     //% weight=50	
+	//% blockGap=7	
 
     export function getLight(): string {
         return light_variable;
@@ -54,6 +55,7 @@ namespace Sensors {
     //% blockId="smarthon_get_temperature" 
     //% block="Get temperature (°C)"
     //% weight=49
+	//% blockGap=7	
 
     export function getTemperature(): string {
         return temperature_variable;
@@ -62,6 +64,7 @@ namespace Sensors {
 	//% blockId="smarthon_get_pressure" 
     //% block="Get pressure (hPa)"
     //% weight=48	
+	//% blockGap=7	
 
     export function getPressure(): string {
         return pressure_variable;
@@ -70,6 +73,7 @@ namespace Sensors {
 	//% blockId="smarthon_get_altitude" 
     //% block="Get altitude (m)"
     //% weight=47	
+	//% blockGap=7	
 
     export function getAltitude(): string {
         return altitude_variable;
@@ -77,15 +81,29 @@ namespace Sensors {
 	
 	//% blockId="smarthon_get_humidity" 
     //% block="Get humidity (%)"
-    //% weight=46	
+    //% weight=46		
 
     export function getHumidity(): string {
         return humidity_variable;
     }
+
+	
+	//% blockId="smarthon_output"
+    //% block="Set output pin %pin| to intensity %intensity"
+    //% intensity.min=0 intensity.max=1023
+    //% weight=45	
+	//% blockGap=7	
+    export function TurnOutput(pin: number, intensity: number): void {
+        
+		serial.writeLine("o/" + pin + "/" + intensity);
+
+    }
+	
+		
     //%blockId=smarthon_at
     //%block="Send AT command %command"
-    //% weight=30	
-    //% blockGap=7		
+    //% weight=44
+    	
     export function sendAT(command: string): void {
         serial.writeLine(command);
     }
